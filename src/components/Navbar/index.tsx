1d278321efb4fc5f12f6f3886c2194b1f8bc3234
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2024-11-12 23:46:43 +0100

    feat(MVP-4): create navbar for desktop and mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,50 @@
 import { ModeTypes } from "@/app/layout";
+import MenuIcon from "@mui/icons-material/Menu";
 import {
   AppBar,
   Box,
   Container,
+  Drawer,
+  IconButton,
   Link as MuiLink,
+  Stack,
   Switch,
   Toolbar,
 } from "@mui/material";
 import Link from "next/link";
-import { Dispatch, FC, SetStateAction } from "react";
+import { usePathname } from "next/navigation";
+import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
 
 interface NavbarProps {
   setMode: Dispatch<SetStateAction<ModeTypes>>;
 }
 
 export const Navbar: FC<NavbarProps> = ({ setMode }) => {
+  const [isDraweOpen, setIsDrawerOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsDrawerOpen(false);
+  }, [pathname]);
+
+  const commonMenu = (
+    <>
+      <MuiLink component={Link} href="/">
+        Home
+      </MuiLink>
+      <MuiLink component={Link} href="/pokemons">
+        Pokemons
+      </MuiLink>
+      <MuiLink component={Link} href="/trainers">
+        Trainers
+      </MuiLink>
+
+      <Switch
+        onClick={() => setMode((prev) => (prev === "dark" ? "light" : "dark"))}
+      />
+    </>
+  );
+
   return (
     <Box
       sx={{
@@ -34,29 +64,66 @@ export const Navbar: FC<NavbarProps> = ({ setMode }) => {
         >
           <Toolbar
             sx={{
-              display: "flex",
               flexDirection: "row",
               justifyContent: "center",
               gap: "2rem",
               width: "100%",
+              display: {
+                xs: "none",
+                sm: "none",
+                md: "flex",
+                lg: "flex",
+                xl: "flex",
+              },
             }}
           >
-            <MuiLink component={Link} href="/">
-              Home
-            </MuiLink>
-            <MuiLink component={Link} href="/pokemons">
-              Pokemons
-            </MuiLink>
-            <MuiLink component={Link} href="/trainers">
-              Trainers
-            </MuiLink>
-
-            <Switch
-              onClick={() =>
-                setMode((prev) => (prev === "dark" ? "light" : "dark"))
-              }
-            />
+            {commonMenu}
           </Toolbar>
+          <IconButton
+            color="primary"
+            size="large"
+            onClick={() => setIsDrawerOpen(true)}
+            sx={{
+              display: {
+                xs: "flex",
+                sm: "flex",
+                md: "none",
+                lg: "none",
+                xl: "none",
+              },
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor="right"
+            open={isDraweOpen}
+            onClose={() => setIsDrawerOpen(false)}
+            sx={{
+              display: {
+                xs: "flex",
+                sm: "flex",
+                md: "flex",
+                lg: "none",
+                xl: "none",
+              },
+            }}
+            PaperProps={{
+              sx: {
+                width: 250,
+              },
+            }}
+          >
+            <Stack
+              direction="column"
+              gap={1}
+              justifyContent={"center"}
+              alignItems={"center"}
+              flexGrow={1}
+            >
+              {commonMenu}
+            </Stack>
+          </Drawer>
         </AppBar>
       </Container>
     </Box>
